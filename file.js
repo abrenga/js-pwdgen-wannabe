7542d1e1bd4de1b5bfe57d596b165f8cbf197b08
a.brenga <antobrg90@hotmail.it>
2024-03-05 23:48:09 +0100

    feat:added info and fixed comments

--- file.js
+++ file.js
@@ -2,12 +2,17 @@
 let nome = prompt("Inserisci il tuo nome");  // restituisce una Stringa o Null
 let cognome = prompt("inserisci il tuo cognome"); // restituisce una Stringa o Null
 let colore = prompt("inserisci un colore"); // restituisce una Stringa o Null
-let randomNum = (Math.random()) * 10; //restituisce un numero base 10
+let randomNum = Math.random() * 100; //restituisce un numero da 0 a 99
 let intRandomNumber = Math.round(randomNum);//restituisce un numero arrotondato
 
 let mostraPw = "La tua password generata è: " + nome + cognome + colore + intRandomNumber;
 console.log(mostraPw);
 document.write(mostraPw);
+
+
+
+
+
 
 
 
