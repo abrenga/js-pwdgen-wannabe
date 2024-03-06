25d5576cc8f9e28db025fcd7d4d331a4a39471b9
Netta90 <58398245+Netta90@users.noreply.github.com>
2024-03-06 18:08:29 +0100

    fixed:  changed let in const

--- file.js
+++ file.js
@@ -1,18 +1,15 @@
 
-let nome = prompt("Inserisci il tuo nome");  // restituisce una Stringa o Null
-let cognome = prompt("inserisci il tuo cognome"); // restituisce una Stringa o Null
-let colore = prompt("inserisci un colore"); // restituisce una Stringa o Null
-let randomNum = Math.random() * 100; //restituisce un numero da 0 a 99
-let intRandomNumber = Math.round(randomNum);//restituisce un numero arrotondato
+/*Preferisco usare const perchè vengono considerati valori costanti quei valori che non cambiano durante
+ tutta l'esecuzione del programma */
+const nome = prompt("Inserisci il tuo nome");  // restituisce una Stringa o Null
+const cognome = prompt("inserisci il tuo cognome"); // restituisce una Stringa o Null
+const colore = prompt("inserisci un colore"); // restituisce una Stringa o Null
+const randomNum = Math.random() * 100; //restituisce un numero da 0 a 99
+const intRandomNumber = Math.round(randomNum);//restituisce un numero arrotondato
 
-let mostraPw = "La tua password generata è: " + nome + cognome + colore + intRandomNumber;
+const mostraPw = "La tua password generata è: " + nome + cognome + colore + intRandomNumber;
 console.log(mostraPw);
 document.write(mostraPw);
-
-
-
-
-
 
 
 
